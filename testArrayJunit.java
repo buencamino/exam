e445b86bcfcd4a332d25d52e8782440bdfb85487
Mike <mlau83work@gmail.com>
2025-08-07 14:00:11 +0800

    update

--- testArrayJunit.java
+++ testArrayJunit.java
@@ -3,15 +3,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class testArrayJunit {
     testArray compare = new testArray();
-    
+
     @Test
-    public void testCompare() {
+    public void testFound() {
         int[] numbers1 = {1, 2, 3, 4, 5, 6};
         int[] numbers2 = {3, 4};
         int result = 0;
 
         result = compare.arrayMatch(numbers1, numbers2);
 
-        assertEquals(result, 2);
+        assertEquals(2, result);
     }
+
+    @Test
+    public void testNotFound() {
+        int[] numbers1 = {1, 2, 3, 4, 5, 6};
+        int[] numbers2 = {3, 6};
+        int result = 0;
+
+        result = compare.arrayMatch(numbers1, numbers2);
+
+        assertEquals(0, result);
+    }
+
 }
